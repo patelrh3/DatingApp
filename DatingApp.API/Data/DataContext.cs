2e8c4efec9e937c9414592c1fb7ddbab79e31ac1
patelrh3 <rishabh.patel144@gmail.com>
2020-05-14 22:35:17 -0400

    Cleaning up

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,6 +1,7 @@
+using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DatingApp.API.Models.Data
+namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
